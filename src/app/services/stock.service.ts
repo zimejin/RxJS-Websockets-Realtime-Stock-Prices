75cb3ce90935037d130118458bc4e0535fdcfec1
Zim <moonspawn@live.co.uk>
2021-11-06 22:21:36 +0100

    fix

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { webSocket } from 'rxjs/webSocket';
 import { Subject } from 'rxjs';
 import { Stock } from '../models/document';
+
 const socket = webSocket("ws://159.89.15.214:8080/");
 
 @Injectable({
@@ -11,14 +12,14 @@ export class StocksService {
   dataRecieved: Subject<Stock> = new Subject();
   listLiveStock: {};
 
-  constructor() { }
+  constructor() {
+    // the observable produces a value once the websocket has been opened
+    socket.subscribe((updates: Stock) => this.dataRecieved.next(updates))
+  }
 
   sendData(isin: string) {
     // request to be sent when the websocket connects
     socket.next({ subscribe: isin });
-
-    // the observable produces a value once the websocket has been opened
-    socket.subscribe((updates: Stock) => this.dataRecieved.next(updates))
   }
 
   connect() {
